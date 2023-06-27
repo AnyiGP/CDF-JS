fdf2da7cfad3fcc54117f10b9e83e6b14df32d73
AnyiGP <angelesgp31@hotmail.com>
2023-06-27 07:44:30 -0300

    Comienzo ejecicios04

--- Ejercicios js/javascript-cdf-main/04.Bucles/ejercicios04.js
+++ Ejercicios js/javascript-cdf-main/04.Bucles/ejercicios04.js
@@ -1,66 +1,83 @@
-function cantidadDeVocales(phrase){
-    // Cuenta la cantidad de vocales en la frase pasada por parametros utilizando un bucle for
-    // Tu código:👇
-    
+function cantidadDeVocales(phrase) {
+  // Cuenta la cantidad de vocales en la frase pasada por parametros utilizando un bucle for
+  // Tu código:👇
+  let contador = 0;
+  for (let i = 0; i < phrase.length; i++) {
+    let letra = phrase[i];
+    if (
+      letra === "a" ||
+      letra === "e" ||
+      letra === "i" ||
+      letra === "o" ||
+      letra === "u"
+    ) {
+      contador++;
     }
-      
-    function primerDivisible(numeroInicial, num) {
-    // Encuentra el primer número divisible por 'num' a partir de un número dado utilizando un bucle while
-    // Tu código:👇
-    
+  }
+
+  return contador;
+}
+
+function primerDivisible(numeroInicial, num) {
+  // Encuentra el primer número divisible por 'num' a partir de un número dado utilizando un bucle while
+  // Tu código:👇
+  // cual es el primerNumeroQueDivide a num iniciiando desde numeroInicial
+  
+  while ((num / numeroInicial) === 'numero entero') {
+    numeroInicial++;
+  }
+  return primerNumeroQueDivide;
+}
+
+function encuentraX(x) {
+  //Realiza un algoritmo que recorre numeros del 1 al 100, cuando pase por el numero X pasado por parametros
+  // retornar "El numero X es: ..."
+  // Tu Código:👇
+  for (let i = 1; i <= 100; i++) {
+    if (i === x) {
+      return `El numero X es: ${i}`;
     }
-    
-    
-    function encuentraX(x){
-    //Realiza un algoritmo que recorre numeros del 1 al 100, cuando pase por el numero X pasado por parametros
-    // retornar "El numero X es: ..."
-    // Tu Código:👇
-    
-    }
-    
-    function sumandoTodo(num){
-    // Realiza un algoritmo que el cual recorra del 0 al numero pasado por parametros, debes retornar la suma de todo los
-    // numeros que se recorrio
-    // Tu código:👇
-    
-    }
-    
-    function BinarioADecimal(num) {
-    // La funcion recibe por parametro, un numero binario pero en tipo de dato String
-    // debes tomar este numero binario y ayudandote de un bucle pasarlo a binario
-    // tu código:👇
-    
-    }
-    
-    function Fibonacci(n){
-    // Realiza una secuencia Fibonacci con el indice pasado por paramentro
-    // Se puede representar mediante la siguiente fórmula: F(n) = F(n-1) + F(n-2)
-    // Tu código:👇
-    
-    }
-    
-    function esPrimo(numero) {
-    //Escribe un programa que verifique si un número dado es primo o no.
-    //Un número primo es aquel que solo es divisible por 1 y por sí mismo.
-    //Si el numero pasado es primo devolve: numero " es primo." , caso contrario: numero " no es primo."
-    // Tu código:👇
-    
-    }
-    
-    function invertirCadena(cadena) {
-    //La función toma una cadena de texto como parámetro, devuelve la cadena invertida.
-    //Ejemplo: 'Hola' -> devolver 'aloH'
-    //Tu codigo:👇
-    
-    }
-      
-    module.exports = {
-        encuentraX,
-        sumandoTodo,
-        BinarioADecimal,
-        Fibonacci,
-        esPrimo,
-        invertirCadena,
-        cantidadDeVocales,
-        primerDivisible
-    }
+  }
+}
+
+function sumandoTodo(num) {
+  // Realiza un algoritmo que el cual recorra del 0 al numero pasado por parametros, debes retornar la suma de todo los
+  // numeros que se recorrio
+  // Tu código:👇
+}
+
+function BinarioADecimal(num) {
+  // La funcion recibe por parametro, un numero binario pero en tipo de dato String
+  // debes tomar este numero binario y ayudandote de un bucle pasarlo a binario
+  // tu código:👇
+}
+
+function Fibonacci(n) {
+  // Realiza una secuencia Fibonacci con el indice pasado por paramentro
+  // Se puede representar mediante la siguiente fórmula: F(n) = F(n-1) + F(n-2)
+  // Tu código:👇
+}
+
+function esPrimo(numero) {
+  //Escribe un programa que verifique si un número dado es primo o no.
+  //Un número primo es aquel que solo es divisible por 1 y por sí mismo.
+  //Si el numero pasado es primo devolve: numero " es primo." , caso contrario: numero " no es primo."
+  // Tu código:👇
+}
+
+function invertirCadena(cadena) {
+  //La función toma una cadena de texto como parámetro, devuelve la cadena invertida.
+  //Ejemplo: 'Hola' -> devolver 'aloH'
+  //Tu codigo:👇
+}
+
+module.exports = {
+  cantidadDeVocales,
+  encuentraX,
+  sumandoTodo,
+  BinarioADecimal,
+  Fibonacci,
+  esPrimo,
+  invertirCadena,
+  primerDivisible,
+};
